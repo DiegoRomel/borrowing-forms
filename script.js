04008b3cfbcbdb29386a1fc10d16633823c5089c
Diego Romel <romeljogo@gmail.com>
2024-08-02 00:34:58 -0400

    atualiza `index.css` e organiza diretório

--- script.js
+++
@@ -1,7 +0,0 @@
-function getInputValue() {
-  const inputElement = document.getElementById("emailInput");
-  const inputValue = inputElement.value;
-  document.getElementById(
-    "displayTest"
-  ).innerText = `input value: ${inputValue}`;
-}
